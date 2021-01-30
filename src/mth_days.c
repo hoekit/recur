9ca69dba6fc6b5f91ba304b8f43550da24e9e5db
Hoe Kit CHEW <hoekit@gmail.com>
2021-01-30 19:43:35 +0700

    Bugfix: Wrap months correctly

--- src/mth_days.c
+++ src/mth_days.c
@@ -2,11 +2,15 @@
 #include "helpers.h"    /* is_leap_year */
 
 // mth_days(2021, 1) == 31          // Month with 31 days
+// mth_days(2020,13) == 31          // 2020-13 equivalent to 2021-01
 // mth_days(2021, 4) == 30          // Month with 30 days
 // mth_days(2000, 2) == 29          // Feb in leap year
 // mth_days(2009, 2) == 28          // Feb in non-leap year
 int mth_days(int year, int month)   // Days in given year-month
 {
+    year  = year + (month-1)/12;
+    month = (month-1)%12 + 1;       // month > 12 translated to years
+
     if ((month == 1)                // Months with 31 days
         || (month ==  3)
         || (month ==  5)
