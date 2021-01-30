ef4d72268331ae1910d6b246d691ca5fb8ccc5a7
Hoe Kit CHEW <hoekit@gmail.com>
2021-01-30 13:28:43 +0700

    Add signature for eom_days

--- helpers.h
+++ helpers.h
@@ -1,3 +1,5 @@
 // helpers.h
 int shift(char *str, char *res);
 int is_leap_year(int year);         // Returns 1 if leap year. Else 0.
+int eom_days(struct tm* lc);        // Days to end of current month
+
